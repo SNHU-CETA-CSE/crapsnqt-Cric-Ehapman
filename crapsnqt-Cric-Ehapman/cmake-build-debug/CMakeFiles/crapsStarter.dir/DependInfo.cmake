f33e5d016e4501646e17ffba1f375ba4aa10f6bf
Eric Chapman <60980956+Cric-Ehapman@users.noreply.github.com>
2020-04-14 23:57:14 -0400

    Added some gui controls and button push stuff

---
+++ crapsnqt-Cric-Ehapman/cmake-build-debug/CMakeFiles/crapsStarter.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/erics/CLionProjects/crapsnqt-Cric-Ehapman/craps.cpp" "C:/Users/erics/CLionProjects/crapsnqt-Cric-Ehapman/cmake-build-debug/CMakeFiles/crapsStarter.dir/craps.cpp.obj"
+  "C:/Users/erics/CLionProjects/crapsnqt-Cric-Ehapman/crapsGame.cpp" "C:/Users/erics/CLionProjects/crapsnqt-Cric-Ehapman/cmake-build-debug/CMakeFiles/crapsStarter.dir/crapsGame.cpp.obj"
+  "C:/Users/erics/CLionProjects/crapsnqt-Cric-Ehapman/cmake-build-debug/crapsStarter_autogen/EWIEGA46WW/qrc_crapsResources.cpp" "C:/Users/erics/CLionProjects/crapsnqt-Cric-Ehapman/cmake-build-debug/CMakeFiles/crapsStarter.dir/crapsStarter_autogen/EWIEGA46WW/qrc_crapsResources.cpp.obj"
+  "C:/Users/erics/CLionProjects/crapsnqt-Cric-Ehapman/cmake-build-debug/crapsStarter_autogen/mocs_compilation.cpp" "C:/Users/erics/CLionProjects/crapsnqt-Cric-Ehapman/cmake-build-debug/CMakeFiles/crapsStarter.dir/crapsStarter_autogen/mocs_compilation.cpp.obj"
+  "C:/Users/erics/CLionProjects/crapsnqt-Cric-Ehapman/die.cpp" "C:/Users/erics/CLionProjects/crapsnqt-Cric-Ehapman/cmake-build-debug/CMakeFiles/crapsStarter.dir/die.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "crapsStarter_autogen/include"
+  "D:/QT/5.14.1/mingw73_64/include"
+  "D:/QT/5.14.1/mingw73_64/include/QtWidgets"
+  "D:/QT/5.14.1/mingw73_64/include/QtGui"
+  "D:/QT/5.14.1/mingw73_64/include/QtCore"
+  "D:/QT/5.14.1/mingw73_64/./mkspecs/win32-g++"
+  "D:/QT/5.14.1/mingw73_64/include/QtANGLE"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
